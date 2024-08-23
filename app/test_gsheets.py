ff942d9bd85f0c48c0632140d2d735dc6e60fae4
dctaf <155950283+dctaf@users.noreply.github.com>
2024-08-23 16:29:27 -0700

    add read_sheet helper and unit test

---
+++ app/test_gsheets.py
@@ -0,0 +1,18 @@
+import unittest
+from pathlib import Path
+
+from gsheets import read_sheet
+
+
+class ReadSheetTest(unittest.TestCase):
+    def test_read_sheet(self):
+        assert (Path.home() / Path(".config/gspread/service_account.json")).exists()
+        # The testing service account has been granted read access to this spreadsheet.
+        test_sheet_url = "https://docs.google.com/spreadsheets/d/redacted/edit?usp=sharing"
+        sheet = read_sheet(test_sheet_url, "Sheet1")
+        assert sheet["col1"][0] == "r1c1"
+        assert sheet["col2"][1] == "r2c2", sheet
+        assert sheet["col2"][2] == "", sheet
+        assert sheet["col3"][2] == "r3c3"
+        assert sheet["col2"][3] == 0
+        assert sheet["col3"][3] == 34.5
