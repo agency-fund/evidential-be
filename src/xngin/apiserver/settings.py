77e09859f4850bc00c6a57bca417c13f811c7b1c
dctaf <155950283+dctaf@users.noreply.github.com>
2024-09-11 12:48:22 -0700

    fixup! Caching. Wiring up test database. Testing cleanup.

--- src/xngin/apiserver/settings.py
+++ src/xngin/apiserver/settings.py
@@ -119,10 +119,11 @@ class SettingsForTesting(XnginSettings):
 
 def get_sqlalchemy_table(sqlat: SqlalchemyAndTable):
     """Connects to a SQLAlchemy DSN and creates a sqlalchemy.Table for introspection."""
+    connect_args = {}
     if sqlat.sqlalchemy_url.startswith("postgres"):
-        connect_args = {"connect_timeout": 5}
-    else:
-        connect_args = {"timeout": 5}
+        connect_args["connect_timeout"] = 5
+    elif sqlat.sqlalchemy_url.startswith("sqlite"):
+        connect_args["timeout"] = 5
     engine = sqlalchemy.create_engine(sqlat.sqlalchemy_url, connect_args=connect_args)
     metadata = sqlalchemy.MetaData()
     return sqlalchemy.Table(sqlat.table_name, metadata, autoload_with=engine)
