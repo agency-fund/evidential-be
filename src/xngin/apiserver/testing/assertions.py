1605a6f1713a06bd3fab1b41b4b5374d00a14bc9
dctaf <155950283+dctaf@users.noreply.github.com>
2024-12-07 09:50:55 -0800

    Hurl(ish) scripts can now contain trailer data.

--- src/xngin/apiserver/testing/assertions.py
+++ src/xngin/apiserver/testing/assertions.py
@@ -3,14 +3,18 @@ from typing import Any
 from deepdiff import DeepDiff
 
 
-def assert_same(actual: Any, expected: Any, extra: str | None = None):
+def assert_same(
+    actual: Any, expected: Any, deepdiff_kwargs=None, extra: str | None = None
+):
     """Compare two values in a float-tolerant way using DeepDiff.
 
     If extra= is set, it will be displayed when the assertions fail.
     """
     # Setting math_epsilon to a non-None value delegates numeric comparisons to
     # https://docs.python.org/3/library/math.html#math.isclose.
-    diff = DeepDiff(actual, expected, math_epsilon=0)
+    if deepdiff_kwargs is None:
+        deepdiff_kwargs = dict()
+    diff = DeepDiff(actual, expected, math_epsilon=0, **deepdiff_kwargs)
     assert not diff, (
         f"Objects differ:\n{diff.pretty()}" + f"\n{extra}\n" if extra else ""
     )
