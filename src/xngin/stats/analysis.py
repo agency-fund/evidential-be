44e2a648709f54079ca4a992f3e6e164add37ea6
roboton <6075595+roboton@users.noreply.github.com>
2025-03-20 06:55:11 +0900

    arm uuid conversion

--- src/xngin/stats/analysis.py
+++ src/xngin/stats/analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import statsmodels.formula.api as smf
 from patsy import EvalFactor
+from uuid import UUID
 from xngin.apiserver.api_types import (
     ParticipantOutcome,
 )
@@ -48,7 +49,8 @@ def analyze_experiment(
         ].categories
         arm_analyses = {}
         for i in range(len(arm_ids)):
-            arm_analyses[arm_ids[i]] = {
+            arm_uuid = UUID(arm_ids[i])
+            arm_analyses[arm_uuid] = {
                 # TODO(roboton): Fix this once we implement #299
                 "is_baseline": i == 0,
                 "estimate": model.params.iloc[i],
