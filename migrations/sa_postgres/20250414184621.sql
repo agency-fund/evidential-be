2ae0e981541154f26969d01e5d580bed0958e6e8
Linus Wong <854794+qixotic@users.noreply.github.com>
2025-04-16 23:15:54 +0800

    Arm table: fix migration by removing a unique constraint for now

--- migrations/sa_postgres/20250414184621.sql
+++ migrations/sa_postgres/20250414184621.sql
@@ -8,7 +8,6 @@ CREATE TABLE "public"."arms" (
   "created_at" timestamptz NOT NULL DEFAULT now(),
   "updated_at" timestamptz NOT NULL DEFAULT now(),
   PRIMARY KEY ("id"),
-  CONSTRAINT "uix_arm_name_org" UNIQUE ("name", "organization_id"),
   CONSTRAINT "arms_experiment_id_fkey" FOREIGN KEY ("experiment_id") REFERENCES "public"."experiments" ("id") ON UPDATE NO ACTION ON DELETE CASCADE,
   CONSTRAINT "arms_organization_id_fkey" FOREIGN KEY ("organization_id") REFERENCES "public"."organizations" ("id") ON UPDATE NO ACTION ON DELETE CASCADE
 );
