1ea435aabf60e1fc28f5e7fe9bf3c4a411a30d82
dctaf <155950283+dctaf@users.noreply.github.com>
2025-07-18 15:33:33 -0700

    Use context manager for app database connection.

---
+++ src/xngin/apiserver/openapi.py
@@ -0,0 +1,75 @@
+import dataclasses
+
+from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
+from fastapi.routing import APIRoute
+
+from xngin.apiserver import flags
+from xngin.apiserver.flags import PUBLISH_ALL_DOCS
+
+
+@dataclasses.dataclass
+class TagDocumentation:
+    visible: bool
+    definition: dict[str, str]
+
+
+def custom_openapi(app: FastAPI):
+    """Customizes the generated OpenAPI schema."""
+    if app.openapi_schema:  # cache
+        return app.openapi_schema
+
+    # Overrides the operationId values in the OpenAPI spec to generate humane names
+    # based on the method name. This avoids generating long, ugly names downstream.
+    # Note: ensure all API methods have names you'd like to appear in the generated APIs.
+    for route in app.routes:
+        if isinstance(route, APIRoute):
+            # uses the Python API method name
+            route.operation_id = route.name
+
+    visible_tags = [
+        TagDocumentation(
+            visible=True,
+            definition={
+                "name": "Stateless Experiment Design",
+                "description": " (⚠️ New clients: use Experiment Integration APIs.) Methods for designing and saving experiments in which a client manages all state persistence.",
+            },
+        ),
+        TagDocumentation(
+            visible=True,
+            definition={
+                "name": "Experiment Integration",
+                "description": "Methods for a client to use when integrating Evidential experiments and assignments with their own serving infrastructure.",
+            },
+        ),
+        TagDocumentation(
+            visible=PUBLISH_ALL_DOCS,
+            definition={"name": "Auth", "description": "Methods for handling SSO."},
+        ),
+        TagDocumentation(
+            visible=PUBLISH_ALL_DOCS,
+            definition={
+                "name": "Admin",
+                "description": "Methods supporting the Evidential UI.",
+            },
+        ),
+    ]
+    openapi_schema = get_openapi(
+        title="xngin: Experiments API",
+        version="0.9.0",
+        contact={
+            "name": "Evidential Developers",
+            "url": flags.XNGIN_PRODUCT_HOMEPAGE,
+            "email": flags.XNGIN_SUPPORT_EMAIL,
+        },
+        summary="",
+        description="",
+        tags=[
+            tag.definition
+            for tag in sorted(visible_tags, key=lambda t: t.definition["name"])
+            if tag.visible
+        ],
+        routes=app.routes,
+    )
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
---
+++ src/xngin/apiserver/routes.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+
+from xngin.apiserver.flags import PUBLISH_ALL_DOCS
+from xngin.apiserver.routers import healthchecks_api
+from xngin.apiserver.routers.admin import admin_api
+from xngin.apiserver.routers.auth import auth_api
+from xngin.apiserver.routers.experiments import experiments_api
+from xngin.apiserver.routers.proxy_mgmt import proxy_mgmt_api
+from xngin.apiserver.routers.stateless import stateless_api
+
+
+def register(app: FastAPI):
+    app.include_router(experiments_api.router, tags=["Experiment Integration"])
+
+    app.include_router(
+        stateless_api.router,
+        tags=["Stateless Experiment Design"],
+    )
+
+    app.include_router(
+        proxy_mgmt_api.router,
+        tags=["Stateless Experiment Design"],
+    )
+
+    app.include_router(
+        healthchecks_api.router, tags=["Health Checks"], include_in_schema=False
+    )
+
+    app.include_router(
+        auth_api.router,
+        tags=["Auth"],
+        include_in_schema=PUBLISH_ALL_DOCS,
+    )
+
+    app.include_router(
+        admin_api.router,
+        tags=["Admin"],
+        include_in_schema=PUBLISH_ALL_DOCS,
+    )
