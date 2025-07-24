7eb26fb39587cdc84549100ca5cc0ed5625916ce
dctaf <155950283+dctaf@users.noreply.github.com>
2025-07-24 11:52:05 -0700

    Remove unused compact_hash method.

--- src/xngin/apiserver/testing/testing_dwh.py
+++ src/xngin/apiserver/testing/testing_dwh.py
@@ -3,7 +3,6 @@
 This corresponds to the "testing" config specified in xngin.testing.settings.json.
 """
 
-import hashlib
 from pathlib import Path
 
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -14,16 +13,6 @@ from xngin.apiserver.settings import Dsn, RemoteDatabaseConfig
 from xngin.apiserver.testing.testing_dwh_def import TESTING_PARTICIPANT_DEF
 
 TESTING_DWH_RAW_DATA = Path(__file__).parent.parent / "testdata/testing_dwh.csv.zst"
-
-
-def compact_hash(path: Path):
-    """Computes a hash of the input CSV so that we can determine whether to re-create the test warehouse."""
-    h = hashlib.blake2b(digest_size=2)
-    with open(path, "rb") as source:
-        h.update(source.read())
-    with open(__file__, "rb") as source:
-        h.update(source.read())
-    return int.from_bytes(h.digest())
 
 
 def create_user_and_first_datasource(
