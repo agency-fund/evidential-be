9ef1a98d0d8bb020f9431c4c7a6df3b8cf04e5d0
poornimaramesh <poornimaramesh1995@gmail.com>
2025-07-26 10:44:33 +0530

    add server default to Experiment table n_trials; Freq/BanditAnalysis -> Freq/BanditAnalysisResponse pydantic models

--- src/xngin/apiserver/routers/admin/test_admin.py
+++ src/xngin/apiserver/routers/admin/test_admin.py
@@ -54,7 +54,7 @@ from xngin.apiserver.routers.common_api_types import (
     DataType,
     DesignSpecMetricRequest,
     ExperimentsType,
-    FreqExperimentAnalysis,
+    FreqExperimentAnalysisResponse,
     GetExperimentAssignmentsResponse,
     GetParticipantAssignmentResponse,
     ListExperimentsResponse,
@@ -882,7 +882,7 @@ async def test_lifecycle_with_db(testing_datasource, ppost, pget, pdelete):
         f"/v1/m/datasources/{testing_datasource.ds.id}/experiments/{parsed_experiment_id}/analyze"
     )
     assert response.status_code == 200, response.content
-    experiment_analysis = FreqExperimentAnalysis.model_validate(response.json())
+    experiment_analysis = FreqExperimentAnalysisResponse.model_validate(response.json())
     assert experiment_analysis.experiment_id == parsed_experiment_id
 
     # Get assignments for the experiment.
@@ -1189,7 +1189,7 @@ def test_experiments_analyze(testing_experiment, pget):
     )
 
     assert response.status_code == 200, response.content
-    experiment_analysis = FreqExperimentAnalysis.model_validate(response.json())
+    experiment_analysis = FreqExperimentAnalysisResponse.model_validate(response.json())
     assert experiment_analysis.experiment_id == experiment_id
     assert len(experiment_analysis.metric_analyses) == 1
     assert experiment_analysis.num_participants == 10
