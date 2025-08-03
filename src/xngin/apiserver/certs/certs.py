b108260117efe3a0b90a5e97bd9e376c8c952598
dctaf <155950283+dctaf@users.noreply.github.com>
2025-08-03 11:08:10 -0700

    Remove some obsolete behaviors from conftest. (#39)
    * Remove conftest.{ensure_correct_working_directory,DeveloperErrorRunFromRootOfRepositoryPleaseError}.
    
    * Dsn.from_url now raises a more useful error message.
    
    * Refine logging of database-related variables during tests.
    
    * Defensively call .resolve() when building paths to files in the repository.

--- src/xngin/apiserver/certs/certs.py
+++ src/xngin/apiserver/certs/certs.py
@@ -2,7 +2,7 @@ import hashlib
 from pathlib import Path
 
 PATH_TO_AMAZON_TRUST_CA_BUNDLE = (
-    (Path(__file__).parent / "amazon-trust-ca-bundle.crt")
+    (Path(__file__).resolve().parent / "amazon-trust-ca-bundle.crt")
     .resolve(strict=True)
     .as_posix()
 )
