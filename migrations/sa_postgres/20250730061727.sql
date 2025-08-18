f366caac04d9537455ca2b1afccead0e6ca9f5bb
poornimaramesh <poornimaramesh1995@gmail.com>
2025-08-18 22:51:50 +0530

    fix migrations

--- migrations/sa_postgres/20250730061727.sql
+++ migrations/sa_postgres/20250730061727.sql
@@ -1,4 +1,4 @@
 -- Rename a column from "context_val" to "context_vals"
 ALTER TABLE "public"."draws" ADD COLUMN "context_vals" double precision[] NULL;
-UPDATE "public"."draws" SET context_vals = ARRAY[context_val] WHERE context_vals IS NULL;
+UPDATE "public"."draws" SET context_vals = context_val WHERE context_vals IS NULL;
 ALTER TABLE "public"."draws" DROP COLUMN context_val;
