9cd6e135cdb6ab28fb3d04b7333682eb2e52c11d
poornimaramesh <poornimaramesh1995@gmail.com>
2025-08-18 23:09:10 +0530

    make context_val column null before dropping it

--- migrations/sa_postgres/20250730061727.sql
+++ migrations/sa_postgres/20250730061727.sql
@@ -1,17 +1,5 @@
 -- Rename a column from "context_val" to "context_vals"
 ALTER TABLE "public"."draws" ADD COLUMN "context_vals" double precision[] NULL;
 UPDATE "public"."draws" SET "context_vals" = "context_val" WHERE "context_val" IS NOT NULL;
-
-DO $$
-BEGIN
-    IF EXISTS (
-        SELECT 1 FROM "public"."draws" 
-        WHERE "context_val" IS NOT NULL 
-        AND ("context_vals" IS NULL OR "context_vals" = '{}')
-        LIMIT 1
-    ) THEN
-        RAISE EXCEPTION 'Data migration incomplete: context_val contains data not migrated to context_vals';
-    END IF;
-END $$;
-
+UPDATE "public"."draws" SET "context_val" = NULL;
 ALTER TABLE "public"."draws" DROP COLUMN "context_val";
