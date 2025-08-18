cb2dcf8fec2b6915de474c65443ccd0f6993b676
poornimaramesh <poornimaramesh1995@gmail.com>
2025-08-18 23:00:34 +0530

    fix migrations again

--- migrations/sa_postgres/20250730061727.sql
+++ migrations/sa_postgres/20250730061727.sql
@@ -1,4 +1,4 @@
 -- Rename a column from "context_val" to "context_vals"
 ALTER TABLE "public"."draws" ADD COLUMN "context_vals" double precision[] NULL;
-UPDATE "public"."draws" SET context_vals = context_val WHERE context_vals IS NULL;
-ALTER TABLE "public"."draws" DROP COLUMN context_val;
+UPDATE "public"."draws" SET "context_vals" = "context_val" WHERE "context_val" IS NOT NULL;
+ALTER TABLE "public"."draws" DROP COLUMN "context_val";
