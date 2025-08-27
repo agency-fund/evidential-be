ab6adc12146b3bb6f5c9f98c0fcbd246aa1e0007
poornimaramesh <poornimaramesh1995@gmail.com>
2025-08-27 11:18:59 +0530

    remove dependency on fixtures

--- src/xngin/stats/test_bandit_sampling.py
+++ src/xngin/stats/test_bandit_sampling.py
@@ -1,45 +1,57 @@
-from xngin.apiserver.conftest import fixture_testing_datasource, fixture_xngin_db_session
+from pydantic import TypeAdapter
+
+from xngin.apiserver.routers.common_api_types import DesignSpec
 from xngin.apiserver.routers.common_enums import ExperimentState, ExperimentsType, LikelihoodTypes, PriorTypes
-from xngin.apiserver.routers.experiments.test_experiments_common import make_insertable_experiment
+from xngin.apiserver.routers.experiments.test_experiments_common import make_createexperimentrequest_json
+from xngin.apiserver.sqla import tables
+from xngin.apiserver.storage.storage_format_converters import ExperimentStorageConverter
 from xngin.stats.bandit_sampling import choose_arm, update_arm
 
-# TODO: hacky way to import fixtures; need to fix
-xngin_session = fixture_xngin_db_session
-testing_datasource = fixture_testing_datasource
 
-
-def test_check_arm_draw_is_reproducible(testing_datasource):
-    beta_binom_experiment, _ = make_insertable_experiment(
-        datasource=testing_datasource.ds,
+def make_experiment_table(
+    experiment_type: ExperimentsType, prior_type: PriorTypes, reward_type: LikelihoodTypes
+) -> tables.Experiment:
+    request = make_createexperimentrequest_json(
+        experiment_type=experiment_type,
+        with_ids=True,
+        prior_type=prior_type,
+        reward_type=reward_type,
+    )
+    design_spec: DesignSpec = TypeAdapter(DesignSpec).validate_python(request["design_spec"])
+    experiment_converter = ExperimentStorageConverter.init_from_components(
+        datasource_id="ds_id",
+        organization_id="org_id",
+        experiment_type=experiment_type,
+        design_spec=design_spec,
         state=ExperimentState.COMMITTED,
-        experiment_type=ExperimentsType.MAB_ONLINE,
-        prior_type=PriorTypes.BETA,
-        reward_type=LikelihoodTypes.BERNOULLI,
+        stopped_assignments_at=None,
+        stopped_assignments_reason=None,
+    )
+    return experiment_converter.get_experiment()
+
+
+def test_check_arm_draw_is_reproducible():
+    tables.Datasource()
+
+    beta_binom_experiment = make_experiment_table(
+        experiment_type=ExperimentsType.MAB_ONLINE, prior_type=PriorTypes.BETA, reward_type=LikelihoodTypes.BERNOULLI
     )
     arm1 = choose_arm(experiment=beta_binom_experiment, random_state=0)
     arm2 = choose_arm(experiment=beta_binom_experiment, random_state=0)
     assert arm1.id == arm2.id
 
-    normal_experiment, _ = make_insertable_experiment(
-        datasource=testing_datasource.ds,
-        state=ExperimentState.COMMITTED,
-        experiment_type=ExperimentsType.MAB_ONLINE,
-        prior_type=PriorTypes.NORMAL,
-        reward_type=LikelihoodTypes.BERNOULLI,
+    normal_experiment = make_experiment_table(
+        experiment_type=ExperimentsType.MAB_ONLINE, prior_type=PriorTypes.NORMAL, reward_type=LikelihoodTypes.BERNOULLI
     )
     arm1 = choose_arm(experiment=normal_experiment, random_state=66)
     arm2 = choose_arm(experiment=normal_experiment, random_state=66)
     assert arm1.id == arm2.id
 
 
-def test_check_arm_drawn_correctly(testing_datasource):
+def test_check_arm_drawn_correctly():
     # Test for Beta prior
-    beta_binom_experiment, _ = make_insertable_experiment(
-        datasource=testing_datasource.ds,
-        state=ExperimentState.COMMITTED,
-        experiment_type=ExperimentsType.MAB_ONLINE,
-        prior_type=PriorTypes.BETA,
-        reward_type=LikelihoodTypes.BERNOULLI,
+    beta_binom_experiment = make_experiment_table(
+        experiment_type=ExperimentsType.MAB_ONLINE, prior_type=PriorTypes.BETA, reward_type=LikelihoodTypes.BERNOULLI
     )
     beta_binom_experiment.arms[0].id = "abcd"
     beta_binom_experiment.arms[1].id = "bcde"
@@ -48,12 +60,8 @@ def test_check_arm_drawn_correctly(testing_datasource):
     assert arm.id == sorted_beta_binom_arms[0].id
 
     # Test for Normal prior
-    normal_experiment, _ = make_insertable_experiment(
-        datasource=testing_datasource.ds,
-        state=ExperimentState.COMMITTED,
-        experiment_type=ExperimentsType.MAB_ONLINE,
-        prior_type=PriorTypes.NORMAL,
-        reward_type=LikelihoodTypes.BERNOULLI,
+    normal_experiment = make_experiment_table(
+        experiment_type=ExperimentsType.MAB_ONLINE, prior_type=PriorTypes.NORMAL, reward_type=LikelihoodTypes.BERNOULLI
     )
     normal_experiment.arms[0].id = "abcd"
     normal_experiment.arms[1].id = "bcde"
@@ -62,14 +70,10 @@ def test_check_arm_drawn_correctly(testing_datasource):
     assert arm.id == sorted_normal_arms[0].id
 
 
-def test_update_arm(testing_datasource):
+def test_update_arm():
     # Test for Beta-binom experiments
-    beta_binom_experiment, _ = make_insertable_experiment(
-        datasource=testing_datasource.ds,
-        state=ExperimentState.COMMITTED,
-        experiment_type=ExperimentsType.MAB_ONLINE,
-        prior_type=PriorTypes.BETA,
-        reward_type=LikelihoodTypes.BERNOULLI,
+    beta_binom_experiment = make_experiment_table(
+        experiment_type=ExperimentsType.MAB_ONLINE, prior_type=PriorTypes.BETA, reward_type=LikelihoodTypes.BERNOULLI
     )
     arm = choose_arm(experiment=beta_binom_experiment, random_state=42)
     alpha, beta = update_arm(experiment=beta_binom_experiment, arm_to_update=arm, outcomes=[0.0])
@@ -79,12 +83,8 @@ def test_update_arm(testing_datasource):
     assert float(arm.beta + 1) == beta
 
     # Test for Normal prior with binary outcomes
-    normal_binary_experiment, _ = make_insertable_experiment(
-        datasource=testing_datasource.ds,
-        state=ExperimentState.COMMITTED,
-        experiment_type=ExperimentsType.MAB_ONLINE,
-        prior_type=PriorTypes.NORMAL,
-        reward_type=LikelihoodTypes.BERNOULLI,
+    normal_binary_experiment = make_experiment_table(
+        experiment_type=ExperimentsType.MAB_ONLINE, prior_type=PriorTypes.NORMAL, reward_type=LikelihoodTypes.BERNOULLI
     )
     arm = choose_arm(experiment=normal_binary_experiment, random_state=42)
     mu, covariance = update_arm(experiment=normal_binary_experiment, arm_to_update=arm, outcomes=[0.0])
@@ -92,9 +92,7 @@ def test_update_arm(testing_datasource):
     assert arm.covariance != covariance
 
     # Test for Normal prior with real-valued outcomes
-    normal_experiment, _ = make_insertable_experiment(
-        datasource=testing_datasource.ds,
-        state=ExperimentState.COMMITTED,
+    normal_experiment = make_experiment_table(
         experiment_type=ExperimentsType.MAB_ONLINE,
         prior_type=PriorTypes.NORMAL,
         reward_type=LikelihoodTypes.NORMAL,
