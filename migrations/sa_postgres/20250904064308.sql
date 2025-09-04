bfa2f22ba04de94e8cc54fc23212041b31a4a17b
poornimaramesh <poornimaramesh1995@gmail.com>
2025-09-04 22:16:34 +0530

    update migrations to not make breaking changes

--- migrations/sa_postgres/20250904064308.sql
+++ migrations/sa_postgres/20250904064308.sql
@@ -1,2 +1,4 @@
+-- Pre-migration check: Ensure observation_type is NULL everywhere
+UPDATE "public"."draws" SET "observation_type" = NULL WHERE "observation_type" IS NOT NULL;
 -- Modify "draws" table
 ALTER TABLE "public"."draws" DROP COLUMN "observation_type";
