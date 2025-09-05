4bcb5a38226a59bc1622624ddee4154d1167a262
poornimaramesh <poornimaramesh1995@gmail.com>
2025-09-05 17:42:58 +0530

    debuggin

--- migrations/sa_postgres/20250905120030.sql
+++ migrations/sa_postgres/20250905120030.sql
@@ -1,6 +1,6 @@
 -- Copy values from "context_val" to "context_vals" if needed
 ALTER TABLE "public"."draws" ADD COLUMN "context_vals" double precision[] NULL;
-UPDATE "public"."draws" SET "context_vals" = ARRAY["context_val"]
+UPDATE "public"."draws" SET "context_vals" = "context_val"
 WHERE "context_val" IS NOT NULL;
 -- Modify "draws" table
 ALTER TABLE "public"."draws" DROP COLUMN "observation_type", DROP COLUMN "context_val";
