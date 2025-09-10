c9e98b94208fd3139a4e0c4c3e3148ba064db546
dctaf <155950283+dctaf@users.noreply.github.com>
2025-09-09 21:52:04 -0700

    Add prefix to session tokens. Lazily acquire key from environment for test ergonomics.

--- src/xngin/apiserver/routers/auth/session_token_crypter.py
+++ src/xngin/apiserver/routers/auth/session_token_crypter.py
@@ -1,11 +1,18 @@
 import json
+import os
 
 from pydantic import ValidationError
 
 from xngin.apiserver import flags
 from xngin.apiserver.routers.auth.principal import Principal
-from xngin.xsecrets.chafernet import Chafernet
+from xngin.xsecrets.chafernet import Chafernet, InvalidTokenError
 from xngin.xsecrets.nacl_provider import NaclProvider, NaclProviderKeyset
+
+SESSION_TOKEN_PREFIX = "xa_"
+
+
+class SessionTokenCrypterMisconfiguredError(Exception):
+    pass
 
 
 class SessionTokenCrypter:
@@ -18,19 +25,25 @@ class SessionTokenCrypter:
     @property
     def _instance(self):
         if not self._chafernet:
-            keys = flags.SESSION_TOKEN_KEYSET
+            keys = os.environ.get(flags.ENV_SESSION_TOKEN_KEYSET, "")
             if not keys:
-                raise RuntimeError(f"{flags.ENV_SESSION_TOKEN_KEYSET} is not set but is required.")
+                raise SessionTokenCrypterMisconfiguredError(
+                    f"{flags.ENV_SESSION_TOKEN_KEYSET} is not set but is required."
+                )
             try:
                 keyset = NaclProviderKeyset.deserialize_base64(keys)
             except ValidationError as err:
-                raise RuntimeError(f"{flags.ENV_SESSION_TOKEN_KEYSET} is invalid") from err
+                raise SessionTokenCrypterMisconfiguredError(f"{flags.ENV_SESSION_TOKEN_KEYSET} is invalid") from err
             self._chafernet = Chafernet(NaclProvider(keyset))
         return self._chafernet
 
     def encrypt(self, principal: Principal):
-        return self._instance.encrypt(json.dumps(principal.model_dump(), separators=(",", ":")), b"")
+        return SESSION_TOKEN_PREFIX + self._instance.encrypt(
+            json.dumps(principal.model_dump(), separators=(",", ":")).encode(), b""
+        )
 
     def decrypt(self, token: str) -> Principal:
-        decrypted = self._instance.decrypt(token, b"", self._ttl)
+        if not token.startswith(SESSION_TOKEN_PREFIX):
+            raise InvalidTokenError
+        decrypted = self._instance.decrypt(token[len(SESSION_TOKEN_PREFIX) :], b"", self._ttl)
         return Principal.model_validate_json(decrypted)
