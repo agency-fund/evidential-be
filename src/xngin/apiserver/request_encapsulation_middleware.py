b3f1dcceb7d6c4611b30664f226fe6388167f325
dctaf <155950283+dctaf@users.noreply.github.com>
2025-10-13 17:22:53 +0900

    Unit test and bug fixes for RequestEncapsulationMiddleware.

--- src/xngin/apiserver/request_encapsulation_middleware.py
+++ src/xngin/apiserver/request_encapsulation_middleware.py
@@ -1,11 +1,16 @@
 import json
 
-import jsonpath.pointer
 import starlette.datastructures
-from fastapi import HTTPException
 from jsonpath import JSONPointer, JSONPointerError
 from starlette.datastructures import Headers
+from starlette.responses import JSONResponse
 from starlette.types import ASGIApp, Message, Receive, Scope, Send
+
+_SUPPORTED_METHODS = {"PATCH", "POST", "PUT"}
+
+
+class RequestEncapsulationBadRequestError(Exception):
+    pass
 
 
 class RequestEncapsulationMiddleware:
@@ -37,11 +42,12 @@ class RequestEncapsulationMiddleware:
             await self._app(scope, receive, send)
             return
         method = scope["method"]
-        if method != "POST":
+        if method not in _SUPPORTED_METHODS:
             await self._app(scope, receive, send)
             return
         headers = Headers(scope=scope)
-        if headers.get("content-type") != "application/json":
+        content_type = headers.get("content-type")
+        if content_type is None or not content_type.lower().startswith("application/json"):
             await self._app(scope, receive, send)
             return
         params = starlette.datastructures.QueryParams(scope["query_string"])
@@ -75,13 +81,18 @@ class _RequestRewriter:
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send):
         self._recv = receive
-        self._send = send
-        await self._app(scope, self.receive, send)
+        try:
+            await self._app(scope, self.receive, send)
+        except RequestEncapsulationBadRequestError as err:
+            resp = JSONResponse({"message": str(err)}, status_code=400)
+            await resp(scope, self._recv, send)
 
     async def receive(self) -> Message:
         message = await self._recv()
         assert message["type"] == "http.request"
         body = message["body"]
+        if body == b"":
+            return message
         more_body = message.get("more_body", False)
         if more_body:
             message = await self._recv()
@@ -91,15 +102,7 @@ class _RequestRewriter:
         parsed = json.loads(body)
         try:
             resolved = JSONPointer(self._unwrap_path).resolve(parsed)
-        except JSONPointerError as e:
-            raise HTTPException(
-                status_code=400,
-                detail=f"{self._unwrap_arg} does not contain a valid JSONPointer path.",
-            ) from e
-        if resolved is jsonpath.pointer.UNDEFINED:
-            raise HTTPException(
-                status_code=400,
-                detail=f"{self._unwrap_arg} refers to an undefined value.",
-            )
+        except JSONPointerError as err:
+            raise RequestEncapsulationBadRequestError(f"{self._unwrap_arg} is not a valid JSONPointer path.") from err
         message["body"] = json.dumps(resolved).encode()
         return message
