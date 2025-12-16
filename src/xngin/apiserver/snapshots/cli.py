1e9c5abfb61c54ca585bdab4401460e4cd4477cd
dctaf <155950283+dctaf@users.noreply.github.com>
2025-12-16 13:16:16 -0800

    Improve metrics and logging for snapshot jobs. (#123)
    * Bump Sentry SDK to 2.44+ for new metrics API.
    
    * Contextualize snapshot log entries with task number.
    
    * Increment counter metrics before and after snapshot.
    
    * Contextualize log lines generated during snapshots.
    
    * Migrate snapshotter to structured (JSON) log output.

--- src/xngin/apiserver/snapshots/cli.py
+++ src/xngin/apiserver/snapshots/cli.py
@@ -2,7 +2,6 @@
 
 import asyncio
 import os
-import sys
 from datetime import timedelta
 from typing import Annotated
 
@@ -32,19 +31,12 @@ async def acollect(snapshot_interval: int, snapshot_timeout: int, parallelism: i
         await snapshotter.create_pending_snapshots(snapshot_interval)
         async with asyncio.TaskGroup() as task:
             for i in range(parallelism):
-                _ = task.create_task(snapshotter.process_pending_snapshots(snapshot_timeout), name=f"sn{i}")
+                with logger.contextualize(task=i):
+                    _ = task.create_task(snapshotter.process_pending_snapshots(snapshot_timeout), name=f"sn{i}")
 
 
 @app.command()
 def collect(
-    log_level: Annotated[
-        str,
-        typer.Option(
-            "--log-level",
-            help="Log level",
-            envvar="LOGURU_LEVEL",
-        ),
-    ] = "DEBUG",
     snapshot_timeout: Annotated[
         int,
         typer.Option(
@@ -66,9 +58,6 @@ def collect(
             "that may occur simultaneously.",
         ),
     ] = NPROC,
-    verbose_exceptions: Annotated[
-        bool, typer.Option(help="Configures loguru to print very detailed stack traces when exceptions occur.")
-    ] = False,
 ):
     """Collect snapshots from the experiments that need them.
 
@@ -80,8 +69,6 @@ def collect(
     consider running this job every hour. This effectively enables hourly retries, allows for non-monotonic clock
     behaviors, and some tolerance for unpredictable cron scheduling or missed invocations.
     """
-    logger.remove()
-    logger.add(sys.stderr, level=log_level, backtrace=verbose_exceptions, diagnose=verbose_exceptions)
     secretservice.setup()
 
     cronjob_monitor_slug = os.environ.get(ENV_CRONJOB_MONITOR_SLUG, "")
