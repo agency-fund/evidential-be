618abe7749de0072b3ef016865c734e780eff89e
poornimaramesh <poornimaramesh1995@gmail.com>
2025-12-31 13:50:51 +0300

    restrict encapsulation middleware to experiments api

--- src/xngin/apiserver/middleware.py
+++ src/xngin/apiserver/middleware.py
@@ -13,4 +13,4 @@ def setup(app):
         allow_origins=["*"],
         max_age=7200,  # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Max-Age
     )
-    app.add_middleware(RequestEncapsulationMiddleware)
+    app.add_middleware(RequestEncapsulationMiddleware, path_prefix="/v1/experiments")
