5699fb11346b2021dcb4cc3fcaf25c322db32c6d
dctaf <155950283+dctaf@users.noreply.github.com>
2026-01-15 22:18:43 -0800

    Move .commit() calls to request handlers. (#136)
    * Modify create_experiment to use a single commit.
    
    * delete handlers now use a single commit
    
    * experiment commit and abandon now use a single commit
    
    * Fix argument type error in a helper method signature.

--- src/xngin/apiserver/routers/admin/admin_api.py
+++ src/xngin/apiserver/routers/admin/admin_api.py
@@ -429,9 +429,11 @@ async def delete_snapshot(
     resource_query = select(tables.Snapshot).where(
         tables.Snapshot.experiment_id == experiment_id, tables.Snapshot.id == snapshot_id
     )
-    return await handle_delete(
+    response = await handle_delete(
         session, allow_missing, authz.is_user_authorized_on_datasource(user, datasource_id), resource_query
     )
+    await session.commit()
+    return response
 
 
 @router.post("/organizations/{organization_id}/datasources/{datasource_id}/experiments/{experiment_id}/snapshots")
@@ -664,12 +666,14 @@ async def delete_webhook_from_organization(
 ):
     """Removes a Webhook from an organization."""
     resource_query = select(tables.Webhook).where(tables.Webhook.id == webhook_id)
-    return await handle_delete(
+    response = await handle_delete(
         session,
         allow_missing,
         authz.is_user_authorized_on_organization(user, organization_id),
         resource_query,
     )
+    await session.commit()
+    return response
 
 
 @router.get("/organizations/{organization_id}/events")
@@ -771,12 +775,14 @@ async def remove_member_from_organization(
         tables.UserOrganization.user_id == user_id,
         tables.UserOrganization.user_id != user.id,  # not current authenticated user
     )
-    return await handle_delete(
+    response = await handle_delete(
         session,
         allow_missing,
         authz.is_user_authorized_on_organization(user, organization_id),
         resource_query,
     )
+    await session.commit()
+    return response
 
 
 @router.patch("/organizations/{organization_id}")
@@ -1037,12 +1043,14 @@ async def delete_datasource(
     The user must be a member of the organization that owns the datasource.
     """
     resource_query = select(tables.Datasource).where(tables.Datasource.id == datasource_id)
-    return await handle_delete(
+    response = await handle_delete(
         session,
         allow_missing,
         authz.is_user_authorized_on_organization(user, organization_id),
         resource_query,
     )
+    await session.commit()
+    return response
 
 
 @router.get("/datasources/{datasource_id}/participants")
@@ -1262,13 +1270,15 @@ async def delete_participant(
         config.participants.remove(participant)
         resource.set_config(config)
 
-    return await handle_delete(
+    response = await handle_delete(
         session,
         allow_missing,
         authz.is_user_authorized_on_datasource(user, datasource_id),
         get_participants_or_none,
         deleter,
     )
+    await session.commit()
+    return response
 
 
 @router.get("/datasources/{datasource_id}/apikeys")
@@ -1336,12 +1346,14 @@ async def delete_api_key(
         .join(tables.Datasource)
         .where(tables.Datasource.id == datasource_id, tables.ApiKey.id == api_key_id)
     )
-    return await handle_delete(
+    response = await handle_delete(
         session,
         allow_missing,
         authz.is_user_authorized_on_datasource(user, datasource_id),
         resource_query,
     )
+    await session.commit()
+    return response
 
 
 @router.post("/datasources/{datasource_id}/experiments")
@@ -1375,7 +1387,7 @@ async def create_experiment(
         session=session, organization_id=organization_id, request_webhooks=body.webhooks
     )
 
-    return await experiments_common.create_experiment_impl(
+    response = await experiments_common.create_experiment_impl(
         request=body,
         datasource=datasource,
         xngin_session=session,
@@ -1384,6 +1396,8 @@ async def create_experiment(
         random_state=random_state,
         validated_webhooks=validated_webhooks,
     )
+    await session.commit()
+    return response
 
 
 @router.get(
@@ -1493,7 +1507,9 @@ async def commit_experiment(
 ):
     ds = await get_datasource_or_raise(session, user, datasource_id)
     experiment = await get_experiment_via_ds_or_raise(session, ds, experiment_id)
-    return await experiments_common.commit_experiment_impl(session, experiment)
+    response = await experiments_common.commit_experiment_impl(session, experiment)
+    await session.commit()
+    return response
 
 
 @router.post(
@@ -1509,7 +1525,9 @@ async def abandon_experiment(
 ):
     ds = await get_datasource_or_raise(session, user, datasource_id)
     experiment = await get_experiment_via_ds_or_raise(session, ds, experiment_id)
-    return await experiments_common.abandon_experiment_impl(session, experiment)
+    response = await experiments_common.abandon_experiment_impl(experiment)
+    await session.commit()
+    return response
 
 
 @router.get("/organizations/{organization_id}/experiments")
@@ -1682,12 +1700,11 @@ async def delete_experiment(
 ):
     """Deletes the experiment with the specified ID."""
     resource_query = select(tables.Experiment).where(tables.Experiment.id == experiment_id)
-    return await handle_delete(
-        session,
-        allow_missing,
-        authz.is_user_authorized_on_datasource(user, datasource_id),
-        resource_query,
+    response = await handle_delete(
+        session, allow_missing, authz.is_user_authorized_on_datasource(user, datasource_id), resource_query
     )
+    await session.commit()
+    return response
 
 
 @router.patch(
