377c743b5300a07b4def08f8a075f4812ef5689f
mdulieu <mdulieu@gmail.com>
2026-01-21 11:21:53 -0800

    Add desired_n to API layer for MDE calculation requests

--- src/xngin/apiserver/routers/admin/admin_api.py
+++ src/xngin/apiserver/routers/admin/admin_api.py
@@ -1793,6 +1793,7 @@ async def power_check(
             power=design_spec.power,
             alpha=design_spec.alpha,
             arm_weights=arm_weights,
+            desired_n=design_spec.desired_n,
         )
     )
 
