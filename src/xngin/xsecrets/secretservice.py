21d7e932cad13fcf60859dc402a1ef0f1f923d8c
dctaf <155950283+dctaf@users.noreply.github.com>
2026-04-07 15:24:34 -0700

    Exempt the CLI from no-op crypter checks.

--- src/xngin/xsecrets/secretservice.py
+++ src/xngin/xsecrets/secretservice.py
@@ -20,7 +20,7 @@ from xngin.xsecrets.provider import Registry
 _SERVICE: SecretService | None = None
 
 
-def setup():
+def setup(*, allow_noop: bool = False):
     """Configures a secrets service according to environment variables."""
     global _SERVICE
 
@@ -33,7 +33,7 @@ def setup():
 
     backend_spec = os.environ.get(ENV_XNGIN_SECRETS_BACKEND, noop_provider.NAME)
     if backend_spec == noop_provider.NAME:
-        if not flags.is_dev_environment():
+        if not flags.is_dev_environment() and not allow_noop:
             raise InvalidSecretStoreConfigurationError(
                 f"{ENV_XNGIN_SECRETS_BACKEND} is unset or set to '{noop_provider.NAME}', which stores secrets "
                 f"in plaintext. This is not allowed outside of development environments. "
