ff836c3ceaf6da0e023f50aef6a9d5f1d95861a7
Poornima Ramesh <poornimaramesh1995@gmail.com>
2026-06-19 14:32:06 +0300

    rename migrations (#290)

--- migrations/sa_postgres/20260607181138.sql
+++
@@ -1,2 +0,0 @@
--- Modify "turn_connections" table
-ALTER TABLE "public"."turn_connections" DROP COLUMN "cached_journeys", DROP COLUMN "cached_journeys_updated_at", ADD COLUMN "journeys_dict" jsonb NULL;
