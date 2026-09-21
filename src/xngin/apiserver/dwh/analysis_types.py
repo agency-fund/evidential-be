7a087df901043ef01f7d82c6f9f80defca74e017
dctaf <155950283+dctaf@users.noreply.github.com>
2026-09-21 23:04:39 +0900

    metric_name uses str instead of FieldName

--- src/xngin/apiserver/dwh/analysis_types.py
+++ src/xngin/apiserver/dwh/analysis_types.py
@@ -1,11 +1,9 @@
 from dataclasses import dataclass
 
-from xngin.apiserver.common_field_types import FieldName
-
 
 @dataclass(slots=True)
 class MetricValue:
-    metric_name: FieldName
+    metric_name: str
     metric_value: float | None
 
 
