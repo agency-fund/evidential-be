ef08d31bd43d15d79c52daf17db97de5776ed444
poornimaramesh <poornimaramesh1995@gmail.com>
2026-09-23 22:57:23 +0300

    add test and update schema

--- src/xngin/apiserver/routers/common_api_types.py
+++ src/xngin/apiserver/routers/common_api_types.py
@@ -1064,6 +1064,8 @@ class BaseBanditExperimentSpec(BaseDesignSpec):
         """
         Validate that the autofail parameters are valid.
         """
+        if self.experiment_type == ExperimentsType.MAB_ONLINE_DWH and self.enable_autofail:
+            raise ValueError(f"Autofail is not supported for {ExperimentsType.MAB_ONLINE_DWH} experiments.")
         if (
             self.enable_autofail
             and self.reward_type == LikelihoodTypes.BERNOULLI
