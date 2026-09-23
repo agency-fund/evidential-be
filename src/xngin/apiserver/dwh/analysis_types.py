096cec8334c8f0508405b66133664055c70f2b80
dctaf <155950283+dctaf@users.noreply.github.com>
2026-09-24 06:43:04 +0900

    Simplify ParticipantOutcome for performance. (#346)
    * get_participant_metrics performance improvements
    
    * metric_name uses str instead of FieldName
    
    * Convert participant metric values to floats

--- src/xngin/apiserver/dwh/analysis_types.py
+++ src/xngin/apiserver/dwh/analysis_types.py
@@ -1,24 +1,13 @@
-from typing import Annotated
-
-from pydantic import BaseModel, Field
-
-from xngin.apiserver.common_field_types import FieldName
-from xngin.apiserver.limits import (
-    MAX_LENGTH_OF_PARTICIPANT_ID_VALUE,
-    MAX_NUMBER_OF_FIELDS,
-)
+from dataclasses import dataclass
 
 
-class MetricValue(BaseModel):
-    metric_name: Annotated[
-        FieldName,
-        Field(
-            description="The field_name from the datasource which this analysis models as the dependent variable (y)."
-        ),
-    ]
-    metric_value: Annotated[float | None, Field(description="The queried value for this field_name.")]
+@dataclass(slots=True)
+class MetricValue:
+    metric_name: str
+    metric_value: float | None
 
 
-class ParticipantOutcome(BaseModel):
-    participant_id: Annotated[str, Field(max_length=MAX_LENGTH_OF_PARTICIPANT_ID_VALUE)]
-    metric_values: Annotated[list[MetricValue], Field(max_length=MAX_NUMBER_OF_FIELDS)]
+@dataclass(slots=True)
+class ParticipantOutcome:
+    participant_id: str
+    metric_values: list[MetricValue]
